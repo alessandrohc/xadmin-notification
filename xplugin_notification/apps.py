c91e675688d96eb4ad976d5316d2eb977a8f4beb
Alessandro Hecht <alessandrohc@gmail.com>
2026-03-06 15:40:54 -0300

    feat(apps): Improve admin verbose name for notifications
    Changes the verbose_name for the notification app to 'Administrative Notifications' to be more descriptive.

--- xplugin_notification/apps.py
+++ xplugin_notification/apps.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 class XpluginNotificationConfig(AppConfig):
 	name = 'xplugin_notification'
-	verbose_name = _('Notifications (admin)')
+	verbose_name = _('Administrative Notifications')
 
 	def ready(self):
 		...
